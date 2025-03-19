e76c85f35587fc1c70def75c872d867cb13d82f7
Matthias Hryniszak <padcom@gmail.com>
2025-03-19 15:40:33 +0100

    Code cleanup

--- src/grapesjs/initialize-editor.ts
+++ src/grapesjs/initialize-editor.ts
@@ -8,11 +8,11 @@ import { example } from './example'
 import { messages } from '../i18n'
 import { useEditor } from '../composables/editor'
 
-export function initialize(gjs: HTMLElement | string) {
+export function initialize(container: HTMLElement | string) {
   const { editor } = useEditor()
 
   editor.value = grapesjs.init({
-    container: gjs,
+    container,
     height: '100dvh',
     plugins: [
       example,
