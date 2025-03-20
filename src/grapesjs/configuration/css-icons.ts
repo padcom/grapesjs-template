ac2e28337a3066808961b2b52871b84ed37a08a2
Matthias Hryniszak <padcom@gmail.com>
2025-03-20 20:53:45 +0100

    Added configuration to load FontAwesome 4.7.0 icons from a different location

---
+++ src/grapesjs/configuration/css-icons.ts
@@ -0,0 +1,1 @@
+export const cssIcons = 'https://unpkg.com/font-awesome@4.7.0/css/font-awesome.min.css'
--- src/grapesjs/configuration/index.ts
+++ src/grapesjs/configuration/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @stylistic/padding-line-between-statements */
 export * from './asset-manager'
+export * from './css-icons'
 export * from './i18n'
 export * from './storage-manager'
